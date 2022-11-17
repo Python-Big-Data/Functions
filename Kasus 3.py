67c25cb70c1423ccec4cdb11d679ed0b0f6de2e9
hp <sahalnurdin@gmail.com>
2022-11-18 06:45:57 +0700

    First Commit

---
+++ Kasus 3.py
@@ -0,0 +1,7 @@
+# Buatlah function findRange(data) yang digunakan untuk menentukan nilai range dari beberapa data berupa bilangan
+# (numbers) yang diberikan pada argumennya. Di mana nilai range adalah nilai maksimum data dikurangi nilai minimum data.
+# Di mana nilai range adalah nilai maksimum data dikurangi nilai minimum data. Misalnya findRange(10, 11, 14, 20), maka
+# function ini akan mengembalikan nilai 10 yang merupakan nilai range data. Nilai ini diperoleh dari pengurangan nilai
+# maksimumnya yaitu 20 terhadap nilai minimumnya yaitu 10.
+
+
