1cbbdd2cd44308077fa102f2dbf78695eb1be90c
hp <sahalnurdin@gmail.com>
2022-11-18 22:38:55 +0700

    Add Changes

---
+++ Proyek Latihan 7.py
@@ -0,0 +1,5 @@
+# Gunakan function-function yang ada di statistik.py untuk mencari rata-rata, nilai maks, dan minimum dari data-data
+# berikut ini:
+# a. 5, 10, 4, 9, 30, 16, 2, 11
+# b. 81, 98, 12, 83, 45, 77, 69, 30, 56
+
