a06f68c95829a40eacf26f1ea156e089fd06008a
hp <sahalnurdin@gmail.com>
2022-11-18 20:20:34 +0700

    Changes

--- Kasus 3.py
+++ Kasus 3.py
@@ -1,7 +1,21 @@
 # Buatlah function findRange(data) yang digunakan untuk menentukan nilai range dari beberapa data berupa bilangan
 # (numbers) yang diberikan pada argumennya. Di mana nilai range adalah nilai maksimum data dikurangi nilai minimum data.
-# Di mana nilai range adalah nilai maksimum data dikurangi nilai minimum data. Misalnya findRange(10, 11, 14, 20), maka
-# function ini akan mengembalikan nilai 10 yang merupakan nilai range data. Nilai ini diperoleh dari pengurangan nilai
-# maksimumnya yaitu 20 terhadap nilai minimumnya yaitu 10.
+# Misalnya findRange(10, 11, 14, 20), maka function ini akan mengembalikan nilai 10 yang merupakan nilai range data.
+# Nilai ini diperoleh dari pengurangan nilai maksimumnya yaitu 20 terhadap nilai minimumnya yaitu 10.
 
 
+def findRange(*myData):
+    # Kita inisiasi atau deklarasikan dari tempat penampungan argumen yang akan di inputkan
+    maks = myData[0]
+    min = myData[0]
+    # Perulangan berikut ini akan mencari nilai maksimum dan minimum dari argumen yang nantinya akan diinputkan
+    for data in myData:
+        if data > maks:
+            maks = data
+        if data < min:
+            min = data
+    # Kita kembalikan nilai agar nilai yang ingin kita cari didapatkan
+    return maks-min
+
+
+print(findRange(5, 1, 4, 6, 10, 33, 12, 6, 9, 13))
--- Kasus 4.py
+++ Kasus 4.py
@@ -1,3 +1,9 @@
 # Buatlah function power(a, n) yang digunakan untuk menentukan nilai dari a^n, di mana a adalah bilangan riil dan n
 # adalah bilangan bulat non negatif. Petunjuk: gunakan konsep rekursif!
 
+# Menggunakan konsep rekursif artinya memanggil variabel sendiri
+def power(a, n):
+    x = a ** n
+    return x
+
+print(power(2, 4))
