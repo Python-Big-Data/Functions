4cc8bfd17008721712c574a98db052b23e4c393b
hp <sahalnurdin@gmail.com>
2022-11-18 21:24:41 +0700

    Add Changes

---
+++ Proyek Latihan 4.py
@@ -0,0 +1,7 @@
+# Buatlah function factorial(n) yang digunakan untuk menghitung nilai bn faktorial. Output dari function ini adalah
+# sebuah bilangan yang merupakan nilai adri n faktorial tersebut.
+
+# Gunakan function tersebut untuk menghitung nilai dari:
+# a. C( 5, 3)
+# b. P (10, 7)
+
