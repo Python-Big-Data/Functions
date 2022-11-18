a4adde3f91bf3dc8908ef9a04aaf47a95283385a
hp <sahalnurdin@gmail.com>
2022-11-18 21:44:48 +0700

    Add Changes

--- Proyek Latihan 4.py
+++ Proyek Latihan 4.py
@@ -1,7 +1,14 @@
-# Buatlah function factorial(n) yang digunakan untuk menghitung nilai bn faktorial. Output dari function ini adalah
-# sebuah bilangan yang merupakan nilai adri n faktorial tersebut.
+# Buatlah function factorial(n) yang digunakan untuk menghitung nilai n faktorial. Output dari function ini adalah
+# sebuah bilangan yang merupakan nilai dari n faktorial tersebut.
 
 # Gunakan function tersebut untuk menghitung nilai dari:
 # a. C( 5, 3)
 # b. P (10, 7)
 
+def factorial(n, fact):
+    for i in range (1, n+1):
+        fact = fact * i
+    return fact
+
+print(factorial(5, 3))
+print(factorial(10, 7))
