c1d6c669c6589f4d592164678625a98e11393973
hp <sahalnurdin@gmail.com>
2022-11-18 21:48:17 +0700

    Add Changes

---
+++ Proyek Latihan 5.py
@@ -0,0 +1,4 @@
+# Buatlah function isPrime(x) yang digunakan untuk menentukan apakah x suatu bilangan prima atau tidak. Function
+# mengembalikan nilai True jika x adalah bilangan prima dan False jika bukan. Bilangan prima adalah bilangan bulat
+# positif lebih besar dari 1 yang hanya tidak memiliki pembagi habis selain 1 dan bilangan itu sendiri.
+
