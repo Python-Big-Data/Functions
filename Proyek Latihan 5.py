ff7aa8562ef1d1f3f85f5e1852df430fac30201f
hp <sahalnurdin@gmail.com>
2022-11-18 21:53:45 +0700

    Add Changes

--- Proyek Latihan 5.py
+++ Proyek Latihan 5.py
@@ -2,3 +2,17 @@
 # mengembalikan nilai True jika x adalah bilangan prima dan False jika bukan. Bilangan prima adalah bilangan bulat
 # positif lebih besar dari 1 yang hanya tidak memiliki pembagi habis selain 1 dan bilangan itu sendiri.
 
+def isPrime(x):
+    if x / x == 1 and x > 1:
+        x = True
+    else:
+        x = False
+    return x
+
+print(isPrime(1))
+print(isPrime(2))
+print(isPrime(3))
+print(isPrime(5))
+print(isPrime(7))
+print(isPrime(11))
+print(isPrime(13))
