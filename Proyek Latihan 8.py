3e31de8e82d43b064bc003cc758b73a41fb9edf1
hp <sahalnurdin@gmail.com>
2022-11-18 23:16:02 +0700

    Add Changes

--- Proyek Latihan 8.py
+++ Proyek Latihan 8.py
@@ -1,4 +1,4 @@
 # Buatlah function sqNumbers(a, b) yang digunakan untuk menampilkan bilangan bulat antara a dan b yang merupakan
-# bilangan kuadrat, Misalkan a = 10 dan b - 50, maka sqNumbers(1, 10) akan menampilkan bilangan 16 25 36 49 yang
+# bilangan kuadrat, Misalkan a = 10 dan b = 50, maka sqNumbers(1, 10) akan menampilkan bilangan 16 25 36 49 yang
 # merupakan bilangan kuadrat antara 10 s/d 50.
 
