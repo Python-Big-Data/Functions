1b92d47068a4b5edd69581c5e977bc43dd5a178a
hp <sahalnurdin@gmail.com>
2022-11-18 23:19:40 +0700

    Add Changes

---
+++ Proyek Latihan 10.py
@@ -0,0 +1,2 @@
+# Buatlah function bin2Dec(s) yang digunakan uintuk mencari bilangan desimal yang merupakan konversi dari bilangan
+# biner s dalam tope data string.
---
+++ Proyek Latihan 9.py
@@ -0,0 +1,4 @@
+# Buatlah function dec2Bin(n) yang digunakan untuk mencaribilangan biner yang merupakan konversi dari bilangan desimal
+# n. Functionini mengembalikan tipe data string yang merupakan bilangan binernya.
+
+
